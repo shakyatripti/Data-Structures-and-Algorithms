b2af4e6c04ddcd7727c98f82aaee7433ce5f873a
Shakya Tripti <shakyatripti1308@gmail.com>
2025-03-08 11:01:04 +0530

    Minimum recolors to get K consecutive black blocks solved

--- Sliding-window/min-recolors-to-get-k-consecutive-black-colors.java
+++ Sliding-window/min-recolors-to-get-k-consecutive-black-colors.java
@@ -1,6 +1,7 @@
 //Problem: https://leetcode.com/problems/minimum-recolors-to-get-k-consecutive-black-blocks/description/
 
 
+//Brute Force Solution
 import java.io.*;
 import java.util.*;
 
@@ -25,5 +26,42 @@ class Main {
         MinRecolors colors = new MinRecolors();
         System.out.println(colors.minRecolorsToGetBlack("WBBWWBBWBW", 7));
         System.out.println(colors.minRecolorsToGetBlack("WBWBBBW", 2));
+    }
+}
+
+
+
+//Optimal Solution
+import java.io.*;
+import java.util.*;
+
+class MinRecolors {
+    public int minRecolorsToGetBlack(String blocks, int k) {
+        int whites = Integer.MAX_VALUE, countWhites = 0;
+        for(int i=0; i<k; i++) {
+            if(blocks.charAt(i)=='W') {
+                countWhites++;
+            }
+        }
+        whites = countWhites;
+        for(int i=k; i<blocks.length(); i++) {
+            if(blocks.charAt(i-k)=='W') {
+                countWhites--;
+            }
+            if(blocks.charAt(i)=='W') {
+                countWhites++;
+            }
+            whites = Math.min(whites, countWhites);
+        }
+        return whites;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        MinRecolors colors = new MinRecolors();
+        System.out.println(colors.minRecolorsToGetBlack("WBBWWBBWBW", 7));
+        System.out.println(colors.minRecolorsToGetBlack("WBWBBBW", 2));
+        System.out.println(colors.minRecolorsToGetBlack("WBWWWWWBBB", 4));
     }
 }
