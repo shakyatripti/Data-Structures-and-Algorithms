05f8402cd445c0de7c3d4f616eea3e66c9fed128
Shakya Tripti <shakyatripti1308@gmail.com>
2025-03-19 17:55:22 +0530

    Find how many times has been an array rotated solved

---
+++ Binary Search/BS-1D Arrays/find-how-many-times-array-been-rotated.java
@@ -0,0 +1,32 @@
+//Problem: https://www.geeksforgeeks.org/problems/rotation4723/1
+
+
+
+import java.io.*;
+import java.util.*;
+
+class RotationPosition {
+    public int findKthRotation(int[] nums) {
+        int l=0, r=nums.length-1;
+        while(l<=r) {
+            int mid = (l+r)/2;
+            if(nums[mid] < nums[r]) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return r;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        int[] nums = {5,4,1,2,3};
+        RotationPosition arr = new RotationPosition();
+        System.out.println(arr.findKthRotation(nums));
+        
+        int[] nums2 = {1,2,3,4,10,23};
+        System.out.println(arr.findKthRotation(nums2));
+    }
+}
