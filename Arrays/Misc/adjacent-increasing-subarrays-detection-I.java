1f358eccd1f744fb3ae928f781b6a130c6c8ed2f
Tripti <shakyatripti1308@gmail.com>
2025-10-14 15:50:04 +0530

    Adjacent increasing subarrays detection I solved

--- Arrays/Misc/adjacent-increasing-subarrays-detection-I.java
+++ Arrays/Misc/adjacent-increasing-subarrays-detection-I.java
@@ -49,3 +49,48 @@ class Main {
         System.out.println(arr.hasIncreasing(nums1, 5));
     }
 }
+
+
+
+
+
+
+
+//Better solution 
+
+
+import java.io.*;
+import java.util.*;
+
+class AdjIncreasingSubarrays {
+    public boolean hasIncreasing(List<Integer> nums, int k) {
+        for(int i=0; i<=nums.size()-2*k; i++) {
+            if(strictlyIncreasing(nums, i, k) && strictlyIncreasing(nums, i+k, k)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean strictlyIncreasing(List<Integer> nums1, int start, int k) {
+        int end = start + k - 1;
+        for(int i=start; i<end; i++) {
+            if(nums1.get(i) >= nums1.get(i+1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+class Main {
+    public static void main(String[] args) {
+        AdjIncreasingSubarrays arr = new AdjIncreasingSubarrays();
+        List<Integer> nums = new ArrayList<>(Arrays.asList(2,5,7,8,9,2,3,4,3,1));
+        System.out.println(arr.hasIncreasing(nums, 3));
+        
+        List<Integer> nums1 = new ArrayList<>(Arrays.asList(1,2,3,4,4,4,4,5,6,7));
+        System.out.println(arr.hasIncreasing(nums1, 5));
+    }
+}
