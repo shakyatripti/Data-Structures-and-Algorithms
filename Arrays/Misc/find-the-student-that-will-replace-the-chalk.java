749c0669666b52eb604ba16f9e4d258f488cf430
Shakya Tripti <shakyatripti1308@gmail.com>
2025-10-31 17:49:59 +0530

    Find the student that will replace the chalk solved

--- Arrays/Misc/find-the-student-that-will-replace-the-chalk.java
+++ Arrays/Misc/find-the-student-that-will-replace-the-chalk.java
@@ -33,3 +33,46 @@ class Main {
         System.out.println(student.chalkReplacer(chalk1, 25));
     }
 }
+
+
+
+
+
+
+//Optimal solution
+
+
+import java.io.*;
+import java.util.*;
+
+class FindTheStudent {
+    public int chalkReplacer(int[] chalk, int k) {
+        long sum = 0;
+        int i = 0;
+        for(i=0; i<chalk.length; i++) {
+            sum+=chalk[i];
+        }
+
+        if(k>=sum) {
+            k=k%(int)sum;
+        }
+
+        for(i=0; i<chalk.length; i++) {
+            if(k < chalk[i]) {
+                break;
+            }
+            k-=chalk[i];
+        }
+        return i;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        FindTheStudent student = new FindTheStudent();
+        int[] chalk = {5,1,5};
+        System.out.println(student.chalkReplacer(chalk, 22));
+        int[] chalk1 = {3,4,1,2};
+        System.out.println(student.chalkReplacer(chalk1, 25));
+    }
+}
