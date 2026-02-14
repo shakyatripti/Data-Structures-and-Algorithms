a9edcbc12a94d5acb60f0462325e059f331d4813
Shakya Tripti <shakyatripti1308@gmail.com>
2026-02-14 15:49:36 +0530

    Count distinct numbers on board solved

---
+++ Math/count-distinct-numbers-on-board.java
@@ -0,0 +1,38 @@
+//Problem: https://leetcode.com/problems/count-distinct-numbers-on-board/description/
+
+
+
+
+//Brute-force solution
+
+import java.io.*;
+import java.util.*;
+
+class CountInBoard {
+    public int distinctIntegers(int n) {
+        Stack<Integer> st = new Stack<>();
+        HashSet<Integer> ans = new HashSet<>();
+        st.add(n);
+        ans.add(n);
+        while(!st.isEmpty()) {
+            for(int i=0; i<st.size(); i++) {
+                int val = st.pop();
+                for(int j=1; j<=val; j++) {
+                    if(val%j==1) {
+                        st.add(j);
+                        ans.add(j);
+                    }
+                }
+            }
+        }
+        return ans.size();
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        CountInBoard board = new CountInBoard();
+        System.out.println(board.distinctIntegers(5));
+        System.out.println(board.distinctIntegers(3));
+    }
+}
