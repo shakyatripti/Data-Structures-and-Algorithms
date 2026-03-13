37ee8c970b09ceb5a402b94cc6c1f8bf6c7b28e2
Shakya Tripti <79310618+shakyatripti@users.noreply.github.com>
2026-03-13 22:01:38 +0530

    Minimum number of seconds to make mountain height zero solved

--- Math/min-number-of-seconds-to-make-mountain-height-zero.java
+++ Math/min-number-of-seconds-to-make-mountain-height-zero.java
@@ -1,3 +1,31 @@
-//Problem: 
+//Problem: https://leetcode.com/problems/minimum-number-of-seconds-to-make-mountain-height-zero/description/
 
 
+
+import java.io.*;
+import java.util.*;
+
+class ReduceToZero {
+    public int numberOfSteps(int num) {
+        int steps = 0;
+        while(num!=0) {
+            steps++;
+            if(num%2==0) {
+                num=num/2;
+            } else {
+                num--;
+            }
+        }
+        return steps;
+    }
+}
+
+
+class Main {
+    public static void main(String[] args) {
+        ReduceToZero arr = new ReduceToZero();
+        System.out.println(arr.numberOfSteps(14));
+        System.out.println(arr.numberOfSteps(8));
+        System.out.println(arr.numberOfSteps(123));
+    }
+}
